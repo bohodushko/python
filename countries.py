bdcd4989a9022778a413dfd53eb8f43f82bbc91d
Ihor Bohodushko <ibohodushko@accesssoftek.com>
2021-11-28 16:09:37 -0800

    add countries.py

---
+++ countries.py
@@ -0,0 +1,3 @@
+countries = [['United States','Washington DC',328.2],['Morocco','Rabat',36.03],['Germany','Berlin',83.02],['India','Delhi',1.353],['China','Beijing',1.393],['France','Paris',66.99]]
+print(countries[2][1])
+print(countries[4][2]/countries[1][2]) 
