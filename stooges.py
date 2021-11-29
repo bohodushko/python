c0d706ff9e57df096d28af451552d88bdb922543
Ihor Bohodushko <ibohodushko@accesssoftek.com>
2021-11-28 16:33:05 -0800

    add +, len

--- stooges.py
+++ stooges.py
@@ -3,3 +3,10 @@ stooges[2] = 'Shemp'
 print(stooges)
 stooges.append('Shemp')
 print(stooges)
+list1 = [1,2]
+list2 = [3,4]
+list = list1 + list2
+print(list)
+print(len(list))
+print(len([1,2]))
+print(len(list[1:2]),' ',list[1:2])
