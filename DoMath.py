064692b3061b84eb1ea3bc97c48b3f4aa72cee9d
Ihor Bohodushko <ibohodushko@accesssoftek.com>
2021-12-12 21:00:25 -0800

    add Domath.py

---
+++ DoMath.py
@@ -0,0 +1,13 @@
+class DoMath:
+
+  def addition(self, x, y):
+    return x + y
+  def subtraction(self, x, y):
+    return x - y
+  def multyply(self, x, y):
+    return x * y
+
+obj = DoMath()
+print(obj.addition(5,6))
+print(obj.subtraction(7,8))
+print(obj.multyply(10,11))
